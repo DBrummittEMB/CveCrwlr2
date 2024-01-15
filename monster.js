adaf22966cabf38afb6d217074e562f3fac0f192
danbrummitt <156483179+danbrummitt@users.noreply.github.com>
2024-01-15 18:19:24 -0500

    Add Monsters
    Added a ton of monsters.

--- monster.js
+++ monster.js
@@ -1,20 +1,267 @@
-export const monsters = [
+export const smallMonsters = [
   {
     name: "Slime",
-    level: 2,
+    level: 1,
     health: 15,
+    power: 5,
     imageUrl: "/imgs/slimeImage.png"
   },
   {
+    name: "Phantom",
+    level: 1,
+    health: 28,
+    power: 8,
+    imageUrl: "/imgs/phantomImage.png"
+  },
+  {
+    name: "Venomous Serpent",
+    level: 1,
+    health: 10,
+    power: 3,
+    imageUrl: "/imgs/venomousSerpentImage.png"
+  },
+  {
+    name: "Prairie Wolf",
+    level: 2,
+    health: 12,
+    imageUrl: "/imgs/prairieWolfImage.png"
+  },
+  {
+    name: "Mountain Viper",
+    level: 2,
+    health: 13,
+    imageUrl: "/imgs/mountainViperImage.png"
+  },
+  {
+    name: "Swamp Toad",
+    level: 2,
+    health: 11,
+    imageUrl: "/imgs/swampToadImage.png"
+  },
+  {
+    name: "Feral Ghoul",
+    level: 3,
+    health: 15,
+    imageUrl: "/imgs/feralGhoulImage.png"
+  },
+  {
+    name: "Dusty Skeleton",
+    level: 3,
+    health: 14,
+    imageUrl: "/imgs/dustySkeletonImage.png"
+  },
+  {
+    name: "Cave Bat",
+    level: 3,
+    health: 9,
+    imageUrl: "/imgs/caveBatImage.png"
+  },
+  {
+    name: "Bog Rat",
+    level: 4,
+    health: 12,
+    imageUrl: "/imgs/bogRatImage.png"
+  },
+  {
+    name: "Forest Imp",
+    level: 4,
+    health: 8,
+    imageUrl: "/imgs/forestImpImage.png"
+  },
+  {
+    name: "Mud Crab",
+    level: 4,
+    health: 10,
+    imageUrl: "/imgs/mudCrabImage.png"
+  },
+  {
+    name: "Dark Elf",
+    level: 5,
+    health: 28,
+    imageUrl: "/imgs/darkElfImage.png"
+  },
+  {
+    name: "Goblin",
+    level: 5,
+    health: 20,
+    imageUrl: "/imgs/goblinImage.png"    
+  },
+  {
+    name: "Orc Warrior",
+    level: 5,
+    health: 30,
+    imageUrl: "/imgs/orcWarriorImage.png"
+  },
+  {
+    name: "Frost Spider",
+    level: 6,
+    health: 16,
+    imageUrl: "/imgs/frostSpiderImage.png"
+  }];
+export const mediumMonsters = [
+  {
     name: "Fanged Beast",
-    level: 8,
+    level: 1,
     health: 60,
+    power: 13,
     imageUrl: "/imgs/fangedBeast.png"
   },
   {
-    name: "Dragon",
-    level: 20,
-    health: 300,
-    imageUrl: "/imgs/bossDragon.png"
-  }
-];
+    name: "Arcane Golem",
+    level: 1,
+    health: 38,
+    power: 18,
+    imageUrl: "/imgs/arcaneGolemImage.png"
+  },
+  {
+    name: "Spectral Assassin",
+    level: 1,
+    health: 50,
+    power: 15,
+    imageUrl: "/imgs/spectralAssassinImage.png"
+  },
+  {
+    name: "Lava Beast",
+    level: 2,
+    health: 40,
+    imageUrl: "/imgs/lavaBeastImage.png"
+  },
+  {
+    name: "Swamp Lurker",
+    level: 2,
+    health: 33,
+    imageUrl: "/imgs/swampLurkerImage.png"
+  },
+  {
+    name: "Frost Giant",
+    level: 2,
+    health: 50,
+    imageUrl: "/imgs/frostGiantImage.png"
+  },
+  {
+    name: "Dune Scorpion",
+    level: 3,
+    health: 26,
+    imageUrl: "/imgs/duneScorpionImage.png"
+  },
+  {
+    name: "Crystal Dragon",
+    level: 3,
+    health: 45,
+    imageUrl: "/imgs/crystalDragonImage.png"
+  },
+  {
+    name: "Mystic Witch",
+    level: 3,
+    health: 24,
+    imageUrl: "/imgs/mysticWitchImage.png"
+  },
+  {
+    name: "Thunder Griffin",
+    level: 4,
+    health: 33,
+    imageUrl: "/imgs/thunderGriffinImage.png"
+  },
+  {
+    name: "Cave Troll",
+    level: 4,
+    health: 50,
+    imageUrl: "/imgs/caveTrollImage.png"
+  },
+  {
+    name: "Fire Elemental",
+    level: 4,
+    health: 25,
+    imageUrl: "/imgs/fireElementalImage.png"
+  },
+  {
+    name: "Ice Wraith",
+    level: 5,
+    health: 22,
+    imageUrl: "/imgs/iceWraithImage.png"
+  },
+  {
+    name: "Shadow Fiend",
+    level: 5,
+    health: 35,
+    imageUrl: "/imgs/shadowFiendImage.png"
+  },
+  {
+    name: "Rock Golem",
+    level: 5,
+    health: 40,
+    imageUrl: "/imgs/rockGolemImage.png"
+  },
+  {
+    name: "Necromancer",
+    level: 6,
+    health: 18,
+    imageUrl: "/imgs/necromancerImage.png"
+  }];
+  export const bossMonsters = [
+    {
+      name: "Dragon",
+      level: 1,
+      health: 300,
+      imageUrl: "/imgs/bossDragon.png"
+    },
+    {
+      name: "Shadow Empress",
+      level: 2,
+      health: 125,
+      imageUrl: "/imgs/shadowEmpressImage.png"
+    },
+    {
+      name: "Solar Phoenix",
+      level: 3,
+      health: 115,
+      imageUrl: "/imgs/solarPhoenixImage.png"
+    },
+    {
+      name: "Netherworld Banshee",
+      level: 4,
+      health: 90,
+      imageUrl: "/imgs/netherworldBansheeImage.png"
+    },
+    {
+      name: "Giant Stone Guardian",
+      level: 5,
+      health: 120,
+      imageUrl: "/imgs/giantStoneGuardianImage.png"
+    },
+    {
+      name: "Stormbringer Wyrm",
+      level: 13,
+      health: 95,
+      imageUrl: "/imgs/stormbringerWyrmImage.png"
+    },
+    {
+      name: "Titan of the Depths",
+      level: 14,
+      health: 105,
+      imageUrl: "/imgs/titanOfTheDepthsImage.png"
+    },
+    {
+      name: "Inferno Dragon",
+      level: 15,
+      health: 110,
+      imageUrl: "/imgs/infernoDragonImage.png"
+    },
+    {
+      name: "Eternal Lich",
+      level: 16,
+      health: 130,
+      imageUrl: "/imgs/eternalLichImage.png"
+    },
+    {
+      name: "Abyssal Overlord",
+      level: 15,
+      health: 120,
+      imageUrl: "/imgs/abyssalOverlordImage.png"
+    },
+    {
+      name: "Celestial Seraph",
+      level: 14,
+      health: 100,
+      imageUrl: "/imgs/celestialSeraphImage.png"
+    }];
