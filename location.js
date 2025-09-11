d7c72ee12e3ed5a989617d582a065084a4b06188
DBrummittEMB <danbrummitt@brummittembroidery.com>
2025-09-11 18:20:34 -0400

    Fix killMonster button functions

--- location.js
+++ location.js
@@ -124,7 +124,7 @@ export const locations = [
     {
       name: "killMonster",
       "button text": ["Go to town square", "Go to town square", "Go to town square"],
-      "button functions": [goTown, goTown, easterEgg],
+      "button functions": [goTown, goTown, goTown],
       text: "The monster screams \"Arg!\" as it dies. You gain experience points and find gold.",
       image: false
     },
