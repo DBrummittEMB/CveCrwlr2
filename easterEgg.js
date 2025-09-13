136bae2065527beb833746bd405829e6a819f91d
DBrummittEMB <danbrummitt@brummittembroidery.com>
2025-09-12 20:41:56 -0400

    Refactor easter egg UI updates

--- easterEgg.js
+++ easterEgg.js
@@ -1,9 +1,9 @@
-import { player, text, goldText, healthText } from './script.js';
+import { player, text } from './script.js';
 import { eventEmitter } from './eventEmitter.js';
 
 export function pickTwo() {
-    pick(2);
-  }
+  pick(2);
+}
 
 /**
  * Picks a random number 8.
@@ -14,23 +14,19 @@ export function pickEight() {
 }
 
 export function pick(guess) {
-    let goldComponent = player.getComponent('gold');
-    let healthComponent = player.getComponent('health');
-    let numbers = [];
-    while (numbers.length < 10) {
-      numbers.push(Math.floor(Math.random() * 11));
-    }
-    text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
-    for (let i = 0; i < 10; i++) {
-      text.innerText += numbers[i] + "\n";
-    }
-    if (numbers.indexOf(guess) !== -1) {
-      text.innerText += "Correct! You win 20 gold!";
-      eventEmitter.emit('addGold', 20);
-      goldText.innerText = goldComponent.gold;
-    } else {
-      text.innerText += "Wrong! You lose 10 health!";
-      eventEmitter.emit('playerDamaged', 10);
-      healthText.innerText = healthComponent.currentHealth;
-    }
+  let numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
   }
+  text.innerText = 'You picked ' + guess + '. Here are the random numbers:\n';
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + '\n';
+  }
+  if (numbers.indexOf(guess) !== -1) {
+    text.innerText += 'Correct! You win 20 gold!';
+    eventEmitter.emit('addGold', 20);
+  } else {
+    text.innerText += 'Wrong! You lose 10 health!';
+    eventEmitter.emit('playerDamaged', 10);
+  }
+}
